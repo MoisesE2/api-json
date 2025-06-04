6890479e0d0f0e99dd6444fc8a89b609c04358dc
Moisés Eduardo <moises.eduardogc@gmail.com>
2025-06-04 17:03:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(Servidor rodando: http://localhost:${port});
+  console.log(`Servidor rodando: http://localhost:${port}`);
 });
